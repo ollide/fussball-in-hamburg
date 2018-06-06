0161c99b2b1e1b92e452464eef889c285ceba850
Oliver Degener <hg.otothel@gmail.com>
2018-06-06 23:08:55 +0200

    extracted MatchCrawlService interface

--- src/main/java/org/ollide/fussifinder/service/MatchCrawlService.java
+++ src/main/java/org/ollide/fussifinder/service/MatchCrawlService.java
@@ -1,42 +1,8 @@
 package org.ollide.fussifinder.service;
 
-import org.ollide.fussifinder.api.MatchClient;
-import org.ollide.fussifinder.model.Team;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
+public interface MatchCrawlService {
 
-import java.io.IOException;
+    String getMatchCalendar(String dateFrom, String dateTo, String zip);
 
-@Service
-public class MatchCrawlService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MatchCrawlService.class);
-
-    private static final String DEFAULT_TEAM_TYPES = Team.getDefaultTeamsQuery();
-
-    private final MatchClient matchClient;
-
-    @Autowired
-    public MatchCrawlService(MatchClient matchClient) {
-        this.matchClient = matchClient;
-    }
-
-    @Cacheable("matchCalendar")
-    public String getMatchCalendar(String dateFrom, String dateTo, String zip) {
-        return getMatchCalendar(dateFrom, dateTo, zip, DEFAULT_TEAM_TYPES);
-    }
-
-    public String getMatchCalendar(String dateFrom, String dateTo, String zip, String teamTypes) {
-        try {
-            LOGGER.debug("Querying match calendar from '{}' to '{}' in ZIP '{}'.", dateFrom, dateTo, zip);
-            return matchClient.matchCalendar(dateFrom, dateTo, zip, teamTypes).execute().body();
-        } catch (IOException e) {
-            LOGGER.error("Failed to query match calendar.", e);
-        }
-        return "";
-    }
-
+    String getMatchCalendar(String dateFrom, String dateTo, String zip, String teamTypes);
 }
