1f84a2b21f259a3883b05eaf3a491b2f10d94650
Oliver Degener <hg.otothel@gmail.com>
2019-01-24 20:01:59 +0100

    MatchCrawlService: Detect 'Load more' in match calendar responses to guarantee complete responses.

---
+++ src/test/java/org/ollide/fussifinder/service/MatchClientCrawlServiceTest.java
@@ -0,0 +1,70 @@
+package org.ollide.fussifinder.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.ollide.fussifinder.ResourceHelper;
+import org.ollide.fussifinder.api.MatchClient;
+import org.ollide.fussifinder.model.AjaxModel;
+import retrofit2.mock.Calls;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class MatchClientCrawlServiceTest {
+
+    private static final String TWO_MATCHES_ZIP = "12345";
+    private static final String TWO_MATCHES_LOAD_MORE_ZIP = "45678";
+
+    private MatchClientCrawlService matchClientCrawlService;
+    private MatchClient matchClient;
+
+    private String response2Matches;
+    private String response2MatchesLoadMore;
+    private String responseLoadMore;
+
+    @Before
+    public void setUp() throws Exception {
+        matchClient = mock(MatchClient.class);
+
+        // 2 Matches
+        response2Matches = ResourceHelper.readMatches("2_matches_2_days.html");
+        when(matchClient.matchCalendar(anyString(), anyString(), eq(TWO_MATCHES_ZIP), anyString()))
+                .thenReturn(Calls.response(response2Matches));
+
+        // 2 Matches load more
+        response2MatchesLoadMore = ResourceHelper.readMatches("2_matches_2_days_load_more.html");
+        when(matchClient.matchCalendar(anyString(), anyString(), eq(TWO_MATCHES_LOAD_MORE_ZIP), anyString()))
+                .thenReturn(Calls.response(response2MatchesLoadMore));
+
+
+        responseLoadMore = ResourceHelper.readMatches("load_more.html");
+
+        AjaxModel ajaxModel = new AjaxModel();
+        ajaxModel.setLastIndex(19);
+        ajaxModel.setSuccess(true);
+        ajaxModel.setFinalResponse(true);
+        ajaxModel.setHtml(responseLoadMore);
+        when(matchClient.loadMoreMatches(anyString(), anyString(), anyString(), anyString(), anyInt()))
+                .thenReturn(Calls.response(ajaxModel));
+
+        matchClientCrawlService = new MatchClientCrawlService(matchClient);
+    }
+
+    @Test
+    public void testGetMatchCalendar() {
+        String matchCalendar = matchClientCrawlService.getMatchCalendar("", "", TWO_MATCHES_ZIP);
+        verify(matchClient, never()).loadMoreMatches(anyString(), anyString(), anyString(), anyString(), anyInt());
+        assertEquals(response2Matches, matchCalendar);
+    }
+
+    @Test
+    public void testGetMatchCalendarLoadMore() {
+        String matchCalendar = matchClientCrawlService.getMatchCalendar("", "", TWO_MATCHES_LOAD_MORE_ZIP);
+        verify(matchClient, times(1))
+                .loadMoreMatches(anyString(), anyString(), eq(TWO_MATCHES_LOAD_MORE_ZIP), anyString(), anyInt());
+
+        // Response contains both HTTP responses -> length == combined length
+        assertEquals(response2MatchesLoadMore.length() + responseLoadMore.length(), matchCalendar.length());
+    }
+}
