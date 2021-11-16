8f81b5942800e4aaf906b9c0b5ad3c365605f57b
Oliver Degener <hg.otothel@gmail.com>
2021-11-16 11:31:34 +0100

    Increase timeouts for Overpass calls

--- src/main/java/org/ollide/fussifinder/config/OverpassConfig.java
+++ src/main/java/org/ollide/fussifinder/config/OverpassConfig.java
@@ -3,11 +3,14 @@ package org.ollide.fussifinder.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import org.ollide.fussifinder.api.OverpassClient;
+import org.ollide.fussifinder.http.HeaderInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class OverpassConfig {
@@ -16,14 +19,23 @@ public class OverpassConfig {
     private String overpassUrl;
 
     @Bean
-    public OverpassClient overpassClient(OkHttpClient client, ObjectMapper objectMapper) {
+    public OverpassClient overpassClient(HeaderInterceptor headerInterceptor, ObjectMapper objectMapper) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(overpassUrl)
                 .addConverterFactory(JacksonConverterFactory.create(objectMapper))
-                .client(client)
+                .client(okHttpClient(headerInterceptor))
                 .build();
 
         return retrofit.create(OverpassClient.class);
+    }
+
+    OkHttpClient okHttpClient(HeaderInterceptor headerInterceptor) {
+        OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+        clientBuilder.readTimeout(180, TimeUnit.SECONDS);
+        clientBuilder.connectTimeout(180, TimeUnit.SECONDS);
+        clientBuilder.callTimeout(180, TimeUnit.SECONDS);
+        clientBuilder.addNetworkInterceptor(headerInterceptor);
+        return clientBuilder.build();
     }
 
 }
