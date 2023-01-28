580e3cff9b59e18ca3a2debda71a052661ecfbc2
Oliver Degener <hg.otothel@gmail.com>
2023-01-28 02:22:23 +0100

    Bump test coverage

--- src/test/java/org/ollide/fussifinder/model/MatchTest.java
+++ src/test/java/org/ollide/fussifinder/model/MatchTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatchTest {
@@ -34,5 +36,23 @@ class MatchTest {
         assertEquals("0", sorted.get(0).getId());
         assertEquals("1", sorted.get(1).getId());
         assertEquals("2", sorted.get(2).getId());
+    }
+
+    @Test
+    void equals() {
+        String id = UUID.randomUUID().toString();
+        Match m1 = new Match();
+        m1.setId(id);
+
+        assertThat(m1).isEqualTo(m1);
+        assertThat(m1).isNotEqualTo(null);
+
+        Match m1Clone = new Match();
+        m1Clone.setId(id);
+        assertThat(m1).isEqualTo(m1Clone);
+
+        Match m2 = new Match();
+        m2.setId(UUID.randomUUID().toString());
+        assertThat(m1).isNotEqualTo(m2);
     }
 }
